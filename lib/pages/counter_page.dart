409aec567623ad35e38b67fd0e784ed8ac6fd756
Gabriella Dajka <dajkagabi01@gmail.com>
2024-06-06 15:11:18 +0200

    eleje

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_element
+
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+//változók
+  int _counter = 0;
+
+//metódusok
+
+  void _incrementCounter() {
+    //setState
+    setState(() {
+      _counter++;
+    });
+  }
+//UI (felhasználó)
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter számláló program",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.lime[100],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //
+            Text(
+              "Megnyomások száma: ",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.blue),
+            ),
+            Text(
+              _counter.toString(),
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
